4e20335e5a1f85e232b3a4cc936f1332ed85185d
draply <doduc0610@gmail.com>
2024-06-06 09:04:08 +0000

    feat: add mint_testNFT

--- src/contracts/mintNftTest.ts
+++ src/contracts/mintNftTest.ts
@@ -3,18 +3,16 @@ import {CHAIN, getTxClient} from "src/global";
 
 export async function mintNftTest(owner: string, contract: string, tokenId: string) {
     let {client, address} = await getTxClient()
-    console.log()
-
     try {
-        const tx = await client.wasmClient.execute(address, contract, {
+        const tx = await client.wasmClient.execute(
+            address,
+            contract, {
                 mint: {
                     token_id: tokenId,
-                    owner: owner,
-                    name: "RustHackathon",
-                    level: 1
+                    owner: owner
                 }
             },
-            5
+            'auto'
         )
         return tx.transactionHash
     } catch (error) {
--- src/tests/routes.test.ts
+++ src/tests/routes.test.ts
@@ -63,55 +63,58 @@ describe('POST /create-account', () => {
 describe('POST /transfer', () => {
     it('should transfer tokens from one account to another', async () => {
         const res = await axios.post('http://localhost:3000/transfer-token', {
-            wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
-            toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
-            name: "unibi",
-            amount: 5,
-        }, {
-            timeout: 100000
-        })
+                wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
+                toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
+                name: "unibi",
+                amount: 5,
+            },
+        )
         const {data} = res;
         expect(res.status).toEqual(200);
         expect(data.txHash).not.toEqual(0);
-    })
+    }, 100000)
     it('send cw20 token', async () => {
         const tx = await axios.post('http://localhost:3000/transfer-token', {
-            wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
-            toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
-            name: "tf/nibi1ty88gpudfh57kqghy62hw8k4nt7765gczu2gqry5gl9ldurrzrns92scum/nubis",
-            amount: 10,
-        }, {
-            timeout: 100000
-        })
+                wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
+                toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
+                name: "tf/nibi1ty88gpudfh57kqghy62hw8k4nt7765gczu2gqry5gl9ldurrzrns92scum/nubis",
+                amount: 10,
+            }
+        )
         const {data} = tx;
         console.log(data.txHash)
-    })
+    }, 100000)
 
 })
 
 describe('POST/ verify', () => {
     it('should verify the create account', async () => {
         const res = await axios.post('http://localhost:3000/verify-create-account', {
-            wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204]
-        }, {
-            timeout: 1000000
-        })
+                wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204]
+            }
+        )
         const {data} = res;
         console.log(data.txHash)
-    })
+    }, 100000)
 })
 
 describe('POST/ transfer NFT', () => {
     it('should transfer NFT from one account to another', async () => {
-        // mintNftTest("nibi1xx6due8t4r5lv0nceld5rdd4ug64nt49g06rpg","nibi1jgpxjfetcgwnncms2dl0tt22dntnp0wtt28w6u5xeqqwfjh0p89spd40pr","5")
-        const tx = await axios.post('http://localhost:3000/send-nft', {
-            wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
-            toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
-            contract: "nibi1jgpxjfetcgwnncms2dl0tt22dntnp0wtt28w6u5xeqqwfjh0p89spd40pr",
-            tokenId: "5"
-        })
+            await mintNftTest("nibi1xx6due8t4r5lv0nceld5rdd4ug64nt49g06rpg", "nibi1jgpxjfetcgwnncms2dl0tt22dntnp0wtt28w6u5xeqqwfjh0p89spd40pr", "9");
+            const tx = await axios.post('http://localhost:3000/send-nft', {
+                wallet_salt: [25, 191, 2, 168, 5, 208, 103, 64, 153, 98, 46, 243, 144, 13, 115, 45, 37, 88, 27, 43, 37, 45, 30, 186, 106, 170, 64, 172, 217, 123, 191, 204],
+                toAddress: "nibi1tsgl9sr8ayy4s9fdf9mr2ck2tptpjy2shdj7ky",
+                contract: "nibi1jgpxjfetcgwnncms2dl0tt22dntnp0wtt28w6u5xeqqwfjh0p89spd40pr",
+                tokenId: "9"
+            })
+        const {data} = tx;
+            console.log(data.txHash)
+        }, 100000
+    )
+    it('mint NFT', async () => {
+        const tx = await mintNftTest("nibi1xx6due8t4r5lv0nceld5rdd4ug64nt49g06rpg", "nibi1jgpxjfetcgwnncms2dl0tt22dntnp0wtt28w6u5xeqqwfjh0p89spd40pr", "10");
         console.log(tx)
-    })
+    }, 100000)
 })
 
 
